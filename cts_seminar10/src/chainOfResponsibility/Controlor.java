68788d4d7792e398ad1002fbc998f67878a3ea21
raduelena07 <elenaradu0712@gmail.com>
2024-04-24 15:51:20 +0300

    cts_seminar10

---
+++ cts_seminar10/src/chainOfResponsibility/Controlor.java
@@ -0,0 +1,8 @@
+package chainOfResponsibility;
+
+public class Controlor extends Platitor {
+    @Override
+    public void plateste(float suma) {
+        System.out.printf("Ai primit o amendă pentru că nu ai plătit.%n");
+    }
+}
